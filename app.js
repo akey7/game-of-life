a0b66ad3fff584db8bbccc393ce902e59013e713
Alicia Key <alicia.key@galvanize.com>
2018-03-01 14:05:35 -0700

    Disables buttons

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
 $(document).ready(function () {
+
+  // SET INITIAL UI STATE
   $('#stop-tick-btn').attr('disabled', true)
+
+  $('#dump-config-btn').click(dumpConfigBtnClick)
+  $('#clear-btn').click(clearBtnClick)
+  $('#one-tick-btn').click(oneTickBtnClick)
+  $('#start-tick-btn').click(startTickBtnClick)
+  $('#stop-tick-btn').click(stopTickBtnClick)
+
+  // BUTTON EVENT HANDLERS
+  function stopTickBtnClick(event) {
+    event.preventDefault()
+    console.log('stopBtnClick()')
+  }
+
+  function oneTickBtnClick(event) {
+    event.preventDefault()
+    console.log('oneTickBtnClick()')
+  }
+
+  function startTickBtnClick(event) {
+    event.preventDefault()
+
+    $('#dump-config-btn').attr('disabled', true)
+    $('#clear-btn').attr('disabled', true)
+    $('#one-tick-btn').attr('disabled', true)
+    $('#start-tick-btn').attr('disabled', true)
+
+    $('#stop-tick-btn').removeAttr('disabled')
+
+    console.log('startTickBtnClick()')
+  }
+
+  function clearBtnClick(event) {
+    event.preventDefault()
+    console.log('clearBtnClick()')
+  }
+
+  function dumpConfigBtnClick(event) {
+    event.preventDefault()
+    console.log('dumpConfigBtnClick()')
+  }
 })
